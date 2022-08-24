4d404fb92a32b75c57389d0c5ea60b6c2503c87a
chaimaa <zharchaimaa1@gmail.com>
2022-08-24 15:40:46 +0100

    hello

---
+++ src/main/java/eai/projectcfr/projectcfr/ProjectcfrApplication.java
@@ -0,0 +1,13 @@
+package eai.projectcfr.projectcfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectcfrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eai/projectcfr/projectcfr/ProjectcfrApplicationTests.java
@@ -0,0 +1,13 @@
+package eai.projectcfr.projectcfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcfrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
